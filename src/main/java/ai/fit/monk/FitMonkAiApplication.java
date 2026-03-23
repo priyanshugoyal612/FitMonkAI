ffc75bb74bd833ac58a140a1de6c33c1d3e5294a
priyanshugoyal612 <priyanshugoyal612@gmail.com>
2026-03-23 15:51:28 +0530

    Initial commit

---
+++ src/main/java/ai/fit/monk/FitMonkAiApplication.java
@@ -0,0 +1,13 @@
+package ai.fit.monk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMonkAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitMonkAiApplication.class, args);
+	}
+
+}
